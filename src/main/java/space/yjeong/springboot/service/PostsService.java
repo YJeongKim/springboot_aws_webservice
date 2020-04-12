d83a7a1e7d436eca3a508e9afc1fa1e1c6d313e7
YJeongKim <yjeong20s@naver.com>
2020-04-12 19:16:18 +0900

    :zap: PostsService, PostsApiController 코드 추가

--- src/main/java/space/yjeong/springboot/service/PostsService.java
+++ src/main/java/space/yjeong/springboot/service/PostsService.java
@@ -3,6 +3,7 @@ package space.yjeong.springboot.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import space.yjeong.springboot.domain.posts.Posts;
 import space.yjeong.springboot.domain.posts.PostsRepository;
 import space.yjeong.springboot.web.dto.PostsSaveRequestDto;
 
@@ -15,5 +16,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
